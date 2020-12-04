fbe60a138dce831615363cd213098e07d07c0341
DarrenN <darren.newton@datadoghq.com>
2020-12-03 19:30:25 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+advent-of-code
+==============
+README text here.
---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "advent-of-code")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/advent-of-code.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(darren.newton))
