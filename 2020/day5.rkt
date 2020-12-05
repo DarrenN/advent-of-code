bec019c1ff121247a450f3593eb79ba8681817b8
DarrenN <info@v25media.com>
2020-12-05 18:34:56 -0500

    Adds solution for Day Five Part Two

--- 2020/day5.rkt
+++ 2020/day5.rkt
@@ -11,15 +11,6 @@
 (define (get-boarding-passes input)
   (string-split (read-input input)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Solution 1
-
-#|
-    BFFFBBFRRR: row 70, column 7, seat ID 567.
-    FFFBBBFRRR: row 14, column 7, seat ID 119.
-    BBFFBBFRLL: row 102, column 4, seat ID 820.
-|#
-
 (define ROWS (for/vector ([i (in-range 128)]) i))
 (define COLS (for/vector ([i (in-range 8)]) i))
 
@@ -27,10 +18,6 @@
 (define (f? c) (char=? #\F c)) ; lower
 (define (r? c) (char=? #\R c)) ; upper
 (define (l? c) (char=? #\L c)) ; lower
-
-(define t1 "FBFBBFF") ;; -> row 44
-(define t2 "RLR") ;; -> col 5
-(define t3 "FBFBBFFRLR")
 
 (define (binary rows input)
   (for/fold ([rs rows])
@@ -51,7 +38,24 @@
     (values (apply string (take cs 7))
             (apply string (drop cs 7)))))
 
-(define (day5 [input "input-5.txt"])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Test data
+
+#|
+    BFFFBBFRRR: row 70, column 7, seat ID 567.
+    FFFBBBFRRR: row 14, column 7, seat ID 119.
+    BBFFBBFRLL: row 102, column 4, seat ID 820.
+|#
+
+(define t1 "FBFBBFF") ;; -> row 44
+(define t2 "RLR") ;; -> col 5
+(define t3 "FBFBBFFRLR")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Solution 1
+
+(define (day5-one [input "input-5.txt"])
   (define passes (get-boarding-passes input))
   (car (sort
         (map (λ (pass)
@@ -61,3 +65,29 @@
                          (vector-ref (binary COLS cs) 0)))
                (+ (* row 8) col))
              passes) >)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Solution 2
+
+(define (day5-two [input "input-5.txt"])
+  (define passes (get-boarding-passes input))
+  (define ids
+    (list->vector
+     (sort (map (λ (pass)
+                  (define-values (rs cs) (split-pass pass))
+                  (define-values (row col)
+                    (values (vector-ref (binary ROWS rs) 0)
+                            (vector-ref (binary COLS cs) 0)))
+                  (+ (* row 8) col))
+                passes) <)))
+  ;; Zip over the IDs comparing the current to the next and if the next
+  ;; isn't equal to current + 1 then there's a gap and the answer should be
+  ;; next - 1
+  (vector-filter
+   (λ (x) x)
+   (for/vector ([idx (in-range (sub1 (vector-length ids)))])
+     (let* ([a (vector-ref ids idx)]
+            [b (vector-ref ids (add1 idx))])
+       (cond
+         [(= (add1 a) b) #f]
+         [else (sub1 b)])))))
