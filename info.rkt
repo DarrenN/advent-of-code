f159808096516fb174da0cc6e8b03f4276a06156
DarrenN <info@v25media.com>
2020-12-05 08:49:09 -0500

    remove superfluous files

--- info.rkt
+++
@@ -1,8 +0,0 @@
-#lang info
-(define collection "advent-of-code")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/advent-of-code.scrbl" ())))
-(define pkg-desc "Advent of Code")
-(define version "0.0")
-(define pkg-authors '(DarrenN))
