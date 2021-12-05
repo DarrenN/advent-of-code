c6d92d7d82562b03772a19140cbc9c58ae6b3614
DarrenN <info@v25media.com>
2021-12-05 07:36:40 -0500

    Working test

--- 2021/4/4a.rkt
+++ 2021/4/4a.rkt
@@ -9,8 +9,8 @@ data structures:
 
 (define board
 (hash 'seen '(.. list of numbers? called)
-    22 (point 0 0 22 #f)
-    12 (point 0 1 12 #f))
+22 (point 0 0 22 #f)
+12 (point 0 1 12 #f))
 
 (define boards (vector ...board))
 
@@ -18,15 +18,22 @@ when a number is "called":
 
 vector-map over each board in boards
 if board has key (called number)
-  update point for key with called? #t and add to 'seen list
-  if (length seen) >= 5 then check for sequences that "win"
+update point for key with called? #t and add to 'seen list
+if (length seen) >= 5 then check for sequences that "win"
 
 check for wins:
 
 
 |#
 
-(require racket/function)
+(require (only-in racket/file
+                  file->lines)
+         (only-in racket/function
+                  curry)
+         (only-in racket/string
+                  string-split)
+         (only-in "../lib/utils.rkt"
+                  group))
 
 (struct point (x y v [called? #:auto #:mutable])
   #:auto-value #f
@@ -35,9 +42,6 @@ check for wins:
 (struct board ([seen #:auto #:mutable] [points #:auto #:mutable])
   #:auto-value (hash)
   #:transparent)
-
-(define (make-board)
-  (board))
 
 ;; hash keyed on point-y
 (define (check-across ps)
@@ -48,6 +52,7 @@ check for wins:
   (for/or ([(_ v) (in-hash sorted)])
     (>= (length v) 5)))
 
+;; hash keyed on point-x
 (define (check-down ps)
   (define sorted
     (for/fold ([h (hash)])
@@ -68,18 +73,65 @@ check for wins:
 (define (add-point b p)
   (set-board-points! b (hash-set (board-points b) (point-v p) p)))
 
+(define (sum-board b n)
+  (define unmarked
+    (for/sum ([p (in-list (hash-values (board-points b)))])
+      (if (not (point-called? p))
+          (point-v p)
+          0)))
+  ;; Return sum of unmarked x last called number
+  (* unmarked n))
+
+;; If the called numbers exists in the board add it to seen
+;; If seen is 5 or greater in length, check board for bingo!
 (define (call-number b n)
-  (when (hash-has-key? (board-points b) n)
-    (set-point-called?! (hash-ref (board-points b) n) #t)
-    (set-board-seen! b (hash-set (board-seen b) n #t))
-    (when (>= (length (hash-keys (board-seen b))) 5)
-      (printf "Winning board? ~a~n" (check-board b)))
-    ))
+  (if (hash-has-key? (board-points b) n)
+      (begin
+        (set-point-called?! (hash-ref (board-points b) n) #t)
+        (set-board-seen! b (hash-set (board-seen b) n #t))
+        (if (>= (length (hash-keys (board-seen b))) 5)
+            (if (check-board b)
+                (sum-board b n)
+                #f)
+            #f))
+      #f))
+
+(define (parse-input input-list)
+  (define calls (car input-list))
+  (define boards
+    (for/fold ([bs '()])
+              ([b (in-list (group (cdr input-list) '() 6))])
+      (cons (map string-split (cdr b)) bs)))
+  (values calls boards))
+
+(define (create-board rows)
+  (define b (board))
+  (for ([row (in-list rows)]
+        [y (in-range (length rows))])
+    (for ([v (in-list row)]
+          [x (in-range (length row))])
+      (void
+       (add-point b (point x y (string->number v))))))
+  b)
+
+(define (create-boards board-list)
+  (for/list ([board (in-list board-list)])
+    (create-board board)))
+
+(define (play-bingo rows calls)
+  (define boards (create-boards rows))
+  (for/or ([call (in-list calls)])
+    (for/or ([board (in-list boards)])
+      (call-number board call))))
+
 
 (module+ main)
 
 (module+ test
-  (require racket/string)
+  (require racket/string
+           (only-in racket/file
+                    file->lines))
+
 
   (define across (for/list ([i (in-range 5)])
                    (point i 2 (* 2 i))))
@@ -95,30 +147,54 @@ check for wins:
   (check-down-or-across c)
 
   (define p (point (random 5) (random 5) (random 50)))
-  (define b (make-board))
+  (define b (board))
   (add-point b p)
 
   (define calls
-    (list (random 50) 11 4 (random 50) (random 50 )16 18 (random 50) 15))
-  
+    (map string->number
+         (string-split "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1" ",")))
+
   (define input #<<BRD
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
 22 13 17 11  0
  8  2 23  4 24
 21  9 14 16  7
  6 10  3 18  5
  1 12 20 15 19
-BRD
-)
 
-  (define board-inputs (map string-split (string-split input "\n")))
-  (define brd (make-board))
-  (for ([row (in-list board-inputs)]
-        [y (in-range (length board-inputs))])
-    (for ([v (in-list row)]
-          [x (in-range (length row))])
-      (void
-       (add-point brd (point x y (string->number v))))))
-  
-  (map (curry call-number brd) calls)
-  
-  )
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+BRD
+    )
+
+  (define rows
+    '((("22" "13" "17" "11" "0")
+       ("8" "2" "23" "4" "24")
+       ("21" "9" "14" "16" "7")
+       ("6" "10" "3" "18" "5")
+       ("1" "12" "20" "15" "19"))
+
+      (("3" "15" "0" "2" "22")
+       ("9" "18" "13" "17" "5")
+       ("19" "8" "7" "25" "23")
+       ("20" "11" "10" "24" "4")
+       ("14" "21" "16" "12" "6"))
+
+      (("14" "21" "17" "24" "4")
+       ("10" "16" "15" "9" "19")
+       ("18" "8" "23" "26" "20")
+       ("22" "11" "13" "6" "5")
+       ("2" "0" "12" "3" "7"))))
+
+  ;; Create boards and run calls through them
+  (play-bingo rows calls))
