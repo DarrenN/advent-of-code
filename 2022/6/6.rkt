3a74f9519c7ca79c3b6cb0fd0d5592f95defdc76
DarrenN <info@v25media.com>
2022-12-15 20:06:05 -0500

    Solves 6B and refactors A too

--- 2022/6/6.rkt
+++ 2022/6/6.rkt
@@ -2,23 +2,23 @@
 
 (require racket/list)
 
-
-(define (is-marker? seen)
-  (cond [(< (length seen) 4) #f]
+(define (is-marker? seen [size 4])
+  (cond [(< (length seen) size) #f]
         [(check-duplicates seen) #f]
         [else #t]))
 
-(define (push seen c)
-  (if (< (length seen) 4)
+(define (push seen c [size 4])
+  (if (< (length seen) size)
       (append seen `(,c))
       (append (drop seen 1) `(,c))))
 
-(define (parse in)
+;; Size denotes marker or message (4 | 14)
+(define (parse in [size 4])
   (define (loop in seen pos)
     (let* ([c (read-char in)]
-           [s (push seen c)])
+           [s (push seen c size)])
       (cond [(eof-object? c) s]
-            [(is-marker? s) (add1 pos)]
+            [(is-marker? s size) (add1 pos)]
             [else (loop in s (add1 pos))])))
   (loop in '() 0))
 
@@ -26,8 +26,7 @@
 ;; Tests
 
 (module+ test
-  (require rackunit
-           (only-in racket/file file->lines))
+  (require rackunit)
 
   (define a (open-input-string "mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
   (define b (open-input-string "bvwbjplbgvbhsrlpgdmjqwftvncz"))
@@ -40,8 +39,19 @@
   (check-eq? (parse c) 6)
   (check-eq? (parse d) 10)
   (check-eq? (parse e) 11)
-  
-  ;(parse a)
-  (call-with-input-file "./input.txt" parse)
 
-  )
+  ;(call-with-input-file "./input.txt" parse)
+
+  (define ma (open-input-string "mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+  (define mb (open-input-string "bvwbjplbgvbhsrlpgdmjqwftvncz"))
+  (define mc (open-input-string "nppdvjthqldpwncqszvftbrmjlhg"))
+  (define md (open-input-string "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+  (define me (open-input-string "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+
+  (check-eq? (parse ma 14) 19)
+  (check-eq? (parse mb 14) 23)
+  (check-eq? (parse mc 14) 23)
+  (check-eq? (parse md 14) 29)
+  (check-eq? (parse me 14) 26)
+
+  (call-with-input-file "./input.txt" (λ (i) (parse i 14))))
