41bbbd56ec29ea17e45abfaac586e17016497836
DarrenN <info@v25media.com>
2023-12-03 16:18:05 -0500

    2023 Day 3

--- 2023/03/3.rkt
+++ 2023/03/3.rkt
@@ -1,6 +1,8 @@
 #lang racket/base
 
-(require racket/string)
+(require racket/hash
+         racket/list
+         racket/string)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 2023 3a
@@ -61,34 +63,122 @@ part - bool? is part or not
   (for/fold ([s 0])
             ([row (in-vector t)]
              [y (in-range (vector-length t))])
-     (define-values (buf part? sum)
-       (for/fold ([buf '()]
-                  [part? #f]
-                  [sum 0]) 
-                 ([pos (in-vector row)]
-                  [x (in-range (vector-length row))])
-         (cond
-           [(char-numeric? pos)
-            (cond
-              ;; we're a part and we're at the end of the line
-              [(and part? (equal? x (sub1 (vector-length row))))
-               (values '() #f
-                       (+ sum
-                          (string->number
-                           (apply string (append buf `(,pos))))))]
-              ;; if part? we don't need to scan neighbors
-              [part? (values (append buf `(,pos)) part? sum)]
-              [else
-               ;; if we have a symbol neighbor then we're a part?
-               (if (has-neighbor? x y)
-                   (values (append buf `(,pos)) #t sum)
-                   (values (append buf `(,pos)) #f sum))])]
-           [(and (not (null? buf)) part?)
-            (values '() #f
-                    (+ sum (string->number (apply string buf))))]
-           [else ;; period or symbol, if we have a buffer & part then sum
-            (values '() #f sum)])))
-     (+ s sum)))
+    (define-values (buf part? sum)
+      (for/fold ([buf '()]
+                 [part? #f]
+                 [sum 0])
+                ([pos (in-vector row)]
+                 [x (in-range (vector-length row))])
+        (cond
+          [(char-numeric? pos)
+           (cond
+             ;; we're a part and we're at the end of the line
+             [(and part? (equal? x (sub1 (vector-length row))))
+              (values '() #f
+                      (+ sum
+                         (string->number
+                          (apply string (append buf `(,pos))))))]
+             ;; if part? we don't need to scan neighbors
+             [part? (values (append buf `(,pos)) part? sum)]
+             [else
+              ;; if we have a symbol neighbor then we're a part?
+              (if (has-neighbor? x y)
+                  (values (append buf `(,pos)) #t sum)
+                  (values (append buf `(,pos)) #f sum))])]
+          [(and (not (null? buf)) part?)
+           (values '() #f
+                   (+ sum (string->number (apply string buf))))]
+          [else ;; period or symbol, if we have a buffer & part then sum
+           (values '() #f sum)])))
+    (+ s sum)))
+
+(struct num (x pos value) #:mutable)
+
+(define (extract-value bs)
+  (for/fold ([v '()]
+             #:result (string->number (apply string v)))
+            ([b (in-list bs)])
+    (append v `(,(third b)))))
+
+;; Part 3b uses a two-pass solution. First we scan the table looking for numbers
+;; which we add to a look-up table (hash) keyed on (x y) coordinates. In the
+;; second pass we look for neighbors of * and pull the values from this lookup
+;; table. This has the downside of eating a lot of space for the lookup, as its
+;; full of dupes.
+(define (find-numbers t)
+  (define h
+    (for/fold ([lookup (hash)])
+              ([row (in-vector t)]
+               [y (in-range (vector-length t))])
+      (define-values (buf look)
+        (for/fold ([buf '()]
+                   [look lookup])
+                  ([pos (in-vector row)]
+                   [x (in-range (vector-length row))])
+          (cond
+            [(and (char-numeric? pos) (equal? x (sub1 (vector-length row))))
+             (let* ([bs (append buf `((,x ,y ,pos)))]
+                    [v (extract-value bs)]
+                    [h (for/hash ([b bs])
+                         (values `(,(car b) ,(cadr b)) v))])
+               (values '() (hash-union look h)))]
+            [(char-numeric? pos)
+             (values (append buf `((,x ,y ,pos))) look)]
+            [(and (not (null? buf)) (not (equal? pos #\*)))
+             (let* ([bs buf]
+                    [v (extract-value bs)]
+                    [h (for/hash ([b bs])
+                         (values `(,(car b) ,(cadr b)) v))])
+               (values '() (hash-union look h)))]
+            ;; handle gear marker
+            [(and (not (null? buf)))
+             (let* ([bs buf]
+                    [v (extract-value bs)]
+                    [h (for/hash ([b bs])
+                         (values `(,(car b) ,(cadr b)) v))])
+               (values '()
+                       (hash-union look h)))]
+            [else (values '() look)])))
+      look))
+  (values t h))
+
+;; Second pass for 3b: looks for gear markers and then pulls any neighbors from
+;; the lookup table by their x,y coordinates. If there are exactly 2
+;; neighbors and they're diff values, multiple them
+(define (find-gears t look)
+  (define neighbor? (find-gear-neighbor t))
+  (define (check-neighbors x y)
+    (filter
+     (λ (x) x)
+     `(,(neighbor? x (sub1 y) ) ;; N
+       ,(neighbor? (add1 x) (sub1 y)) ;; NE
+       ,(neighbor? (add1 x) y) ;; E
+       ,(neighbor? (add1 x) (add1 y)) ;; SE
+       ,(neighbor? x (add1 y)) ;; S
+       ,(neighbor? (sub1 x) (add1 y)) ;; SW
+       ,(neighbor? (sub1 x) y) ;; W
+       ,(neighbor? (sub1 x) (sub1 y)))))
+
+  (for/fold ([gs '()])
+            ([row (in-vector t)]
+             [y (in-range (vector-length t))])
+    (define g
+      (for/fold ([gears '()])
+                ([pos (in-vector row)]
+                 [x (in-range (vector-length row))])
+        (cond
+          [(equal? pos #\*)
+           (let* ([ns (check-neighbors x y)]
+                  [vs (for/hash ([n (in-list ns)])
+                        (values (hash-ref look `(,(car n) ,(cadr n))) #t))]
+                  [ks (hash-keys vs)])
+             (if (and (> (length ks) 1)
+                      (< (length ks) 3)
+                      (not (eq? (car ks) (cadr ks))))
+                 (append gears `(,(* (car ks) (cadr ks))))
+                 gears))]
+          [else gears])))
+    (append gs g)))
 
 ;; Predicate for not numeric and not a .
 (define (part-symbol? c)
@@ -104,6 +194,16 @@ part - bool? is part or not
       [(or (< x 0) (< y 0) (> x (sub1 max-col)) (> y (sub1 max-row))) #f]
       [else (part-symbol? (vector-ref (vector-ref t y) x))])))
 
+(define (find-gear-neighbor t)
+  (define max-row (vector-length t))
+  (define max-col (vector-length (vector-ref t 0)))
+  (λ (x y)
+    (cond
+      [(or (< x 0) (< y 0) (> x (sub1 max-col)) (> y (sub1 max-row))) #f]
+      [else
+       (if (char-numeric? (vector-ref (vector-ref t y) x))
+           `(,x ,y)
+           #f)])))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tests
@@ -118,9 +218,10 @@ part - bool? is part or not
   (define (prep-test-input s)
     (string-split s #px"\n"))
 
-  (define test-input "467..114..
+  (define test-input "
+467..114..
 ...*......
-..35...633
+..35..633.
 ......#...
 617*......
 .....+.58.
@@ -129,13 +230,12 @@ part - bool? is part or not
 ...$.*....
 .664.598..")
 
-(define test-input2 ".78@......../..........422....444.............*...............281............*...*....447..37.......*................814....................
-........./...788...810.........&..............369..985....-................92................................684...............74...........")
-  
-(find-parts (parse-input (prep-test-input test-input)))
+  ;; 3b
+  (define-values (t look)
+    (find-numbers (parse-input (file->lines "./input.txt"))))
+  (apply + (find-gears t look))
 
-(find-parts (parse-input (file->lines "./input.txt")))
-
-;;(apply + )
+  ;; 3a
+  ;;(find-parts (parse-input (file->lines "./input.txt")))
 
   )
